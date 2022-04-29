2754d2329cf3c1c06db2c855b5da53a102583aef
bakpawel <pawel.bak@ttpsc.pl>
2022-04-29 15:13:36 +0200

    Added cors

--- src/main/java/com/example/knowme/controller/QuestionController.java
+++ src/main/java/com/example/knowme/controller/QuestionController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(maxAge = 3600)
 public class QuestionController {
     @Autowired()
     private QuestionRepository questionRepository;
