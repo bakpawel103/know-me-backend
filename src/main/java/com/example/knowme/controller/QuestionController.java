62e66881321c16d696d0d3df5e80cdcf8a8b0024
bakpawel <pawel.bak@ttpsc.pl>
2022-05-09 19:20:14 +0200

    [*] changes in cors

--- src/main/java/com/example/knowme/controller/QuestionController.java
+++ src/main/java/com/example/knowme/controller/QuestionController.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1/")
 public class QuestionController {
